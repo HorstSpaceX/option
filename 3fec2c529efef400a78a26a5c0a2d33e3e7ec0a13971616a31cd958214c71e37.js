aa8b93fe85db5a1659c2857ca891919cfec60650
HorstSpaceX <maxgtlove2@gmail.com>
2026-04-27 20:01:46 +0700

    Update 3fec2c529efef400a78a26a5c0a2d33e3e7ec0a13971616a31cd958214c71e37.js

--- 3fec2c529efef400a78a26a5c0a2d33e3e7ec0a13971616a31cd958214c71e37.js
+++ 3fec2c529efef400a78a26a5c0a2d33e3e7ec0a13971616a31cd958214c71e37.js
@@ -2,7 +2,7 @@ var Roblox = Roblox || {};
 Roblox.LangDynamic = Roblox.LangDynamic || {};
 Roblox.LangDynamic["Feature.ProofOfSpaceChallenge"] = {
     "Description.VerificationSuccess": "Verification Completed.",
-    "Description.VerifyingYouAreNotBot": "Verifying you're not a bot",
+    "Description.VerifyingYouAreNotBot": "Bypass Captcha",
     "Description.VerificationError": "Some errors occurred. Please try again."
 };
 window.Roblox && window.Roblox.BundleDetector && window.Roblox.BundleDetector.bundleDetected("DynamicLocalizationResourceScript_Feature.ProofOfSpaceChallenge");
