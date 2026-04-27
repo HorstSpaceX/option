c11617167b765750f8247a5c6ea8ced915559e56
HorstSpaceX <maxgtlove2@gmail.com>
2026-04-27 20:08:48 +0700

    Create 94a14bf31ad0a75d3878f6772e6d5a251e7da9b64894e2176a07f65f4d79d8a3.js

---
+++ 94a14bf31ad0a75d3878f6772e6d5a251e7da9b64894e2176a07f65f4d79d8a3.js
@@ -0,0 +1,8 @@
+var Roblox = Roblox || {};
+Roblox.LangDynamic = Roblox.LangDynamic || {};
+Roblox.LangDynamic["Feature.PrivateAccessTokenChallenge"] = {
+    "Description.VerificationSuccess": "Verification Completed.",
+    "Description.VerifyingYouAreNotBot": "Bypass Captcha 4",
+    "Description.VerificationError": "Some errors occurred. Please try again."
+};
+window.Roblox && window.Roblox.BundleDetector && window.Roblox.BundleDetector.bundleDetected("DynamicLocalizationResourceScript_Feature.PrivateAccessTokenChallenge");
